8acfeddae8c5fc1b386093f0159e492674de3b56
Nathan Alder <nathan.alder@gmail.com>
2024-01-24 21:19:23 +0100

    feat: update locales based on changes in strapi

--- frontend/i18n-config.ts
+++ frontend/i18n-config.ts
@@ -1,6 +1,6 @@
 export const i18n = {
-    defaultLocale: 'en',
-    locales: ['en', 'de', 'nl'],
+  defaultLocale: "en",
+  locales: ["en", "de-DE", "de-CH", "nl"],
 } as const;
 
-export type Locale = typeof i18n['locales'][number];
+export type Locale = (typeof i18n)["locales"][number];
